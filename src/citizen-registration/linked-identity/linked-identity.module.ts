2239b4caff45a93ef6023f126c4cacc48817d9a8
Alfred-Adeyiga <92817840+Alfred-Adeyiga@users.noreply.github.com>
2022-02-25 15:43:09 +0100

    Final

--- src/citizen-registration/linked-identity/linked-identity.module.ts
+++ src/citizen-registration/linked-identity/linked-identity.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LinkedIdentityService } from './linked-identity.service';
 import { LinkedIdentityController } from './linked-identity.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BioDatum } from '../bio-data/entities/bio-datum.entity';
+import { LinkedIdentity } from './entities/linked-identity.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BioDatum, LinkedIdentity])],
   controllers: [LinkedIdentityController],
   providers: [LinkedIdentityService]
 })
